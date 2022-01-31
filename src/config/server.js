4611ab6eb33b8a819da316e782d0d9e382321ef2
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-01-30 21:04:32 -0300

    Deleted unnecessary dependency body-parser in server.js

--- src/config/server.js
+++ src/config/server.js
@@ -2,10 +2,10 @@ const swaggerUI = require("swagger-ui-express");
 const swaggerDocs = require("../../swagger.json");
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.json());
 app.use(express.json());
 app.set("port", 3000);
 
@@ -14,4 +14,4 @@ app.use("/film", require("../film-api/route/filmRoute"));
 app.use("/user", require("../film-api/route/userRoute"));
 app.use("/security", require("../film-api/route/loginRoute"));
 
-module.exports = app
+module.exports = app;
